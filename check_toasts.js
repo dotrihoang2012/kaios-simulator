2878f57326068853563356e66f05c973e6dee93c
Do Tri Hoang <dotrihoang2012@gmail.com>
2026-09-25 23:21:21 +0700

    Delete check_toasts.js

--- check_toasts.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let js = fs.readFileSync('web-settings/settings/js/panels/wifi_auth/panel.js', 'utf8');
-if (js.includes('connectNetwork called')) {
-    console.log('Toasts are in panel.js');
-} else {
-    console.log('Toasts MISSING from panel.js');
-}
